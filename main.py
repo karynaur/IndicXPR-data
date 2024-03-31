5e442f7dc3320f08e5d69cadfa4e0c50a224d1c7
Aditya Srinivas Menon <adityasrinivas.menon@gmail.com>
2024-03-31 11:47:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from phrase_extract import main
 
 src_lang = "eng_Latn"
 languages = ["tel_Telu", "kan_Knda", "tam_Taml", "hin_Deva", "mal_Mlym"]
-
+tgt_lang = "mal_Mlym"
 
 # Specify source and target files
-src_file = 'data/'  # Change this to your source file
-output_file = 'out/'  # Change this to your desired output file
+src_file = 'data/en-ml/'  # Change this to your source file
+output_folder = 'out/'  # Change this to your desired output file
 
-for lang in languages:
-    main(src_file, src_lang, lang, output_file)
+main(src_file, src_lang, tgt_lang, output_folder)
